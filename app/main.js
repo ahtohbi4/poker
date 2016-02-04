91b539b22d5a255ba3062e50e0b2ec571d85a898
Alexander Antonov <alexandr-post@yandex.ru>
2016-02-05 01:56:31 +0300

    Poker was created

--- app/main.js
+++ app/main.js
@@ -1,42 +1,7 @@
 require([
     'jquery',
-    '../app/cardDeck'
-], function ($, CardDeck) {
-        var cardDeck = new CardDeck(36);
-
-        cardDeck.reset();
-
-        $('body')
-            .append(cardDeck.cards.join(', '))
-            .append('<hr>');
-
-        cardDeck.shuffle();
-
-        $('body')
-            .append(cardDeck.cards.join(', '))
-            .append('<hr>');
-
-        var cardOnHand = [];
-        for(var i = 0; i < 3; i++) {
-            cardOnHand.push(cardDeck.getCard());
-        }
-
-        $('body')
-            .append(cardOnHand.join(', '))
-            .append('<br>')
-            .append(cardDeck.cards.join(', '))
-            .append('<hr>');
-
-        $('body')
-            .append(cardDeck.getCards(6).join(', '))
-            .append('<br>')
-            .append(cardDeck.cards.join(', '))
-            .append('<hr>');
-
-        cardDeck.reset();
-
-        $('body')
-            .append(cardDeck.cards.join(', '))
-            .append('<hr>');
+    '../app/poker'
+], function ($, Poker, CardDeck) {
+        var poker = new Poker();
     }
 );
---
+++ app/poker.js
@@ -0,0 +1,113 @@
+define([
+    'jquery',
+    '../app/cardDeck'
+], function ($, cardDeck) {
+    'use strict';
+
+    var Poker = function () {
+        this.reset(10000000);
+    };
+
+    Poker.prototype.getElem = function (elem) {
+        return {
+            count: $('.poker__count'),
+            hand: $('.poker__hand')
+        }[elem] || null;
+    };
+
+    Poker.prototype.reset = function(deposit) {
+        this.deposit = deposit || 1000;
+
+        this.getElem('count').text(this.deposit);
+    };
+
+    /**
+     * @method to check for "Straight flush"
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isStraightFlash = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * Four of a kind
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isFourOfKind = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * Full house
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isFullHouse = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * Flush
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isFlush = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * Straight
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isStraight = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * Three of a kind
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isThreeOfKind = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * Two pair
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isTwoPair = function() {
+        var result;
+
+        return result;
+    };
+
+    /**
+     * One pair
+     * @returns {boolean}
+     * @private
+     */
+    Poker.prototype._isOnePair = function() {
+        var result;
+
+        return result;
+    };
+
+    return Poker;
+});
