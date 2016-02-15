89c6621c0e6bcc7f507941db603b0e048a3a00ae
Alexander Antonov <alexandr-post@yandex.ru>
2016-02-15 23:48:43 +0300

    BDD unit tests was added

--- spec/hands.spec.js
+++ spec/hands.spec.js
@@ -7,26 +7,38 @@ define([
 
     describe('Checking Hands with method _isOnePair()', function () {
         describe('without repeating Cards', function () {
-            poker.hands = [
-                new Card({ value: 10, suit: '♥' }),
-                new Card({ value: 3,  suit: '♥' }),
-                new Card({ value: 2,  suit: '♦' }),
-                new Card({ value: 5,  suit: '♦' }),
-                new Card({ value: 6,  suit: '♠' })
-            ]
             it('should returns false', function () {
+                poker.board = [
+                    new Card({ value: 10, suit: '♥' }),
+                    new Card({ value: 3,  suit: '♥' }),
+                    new Card({ value: 2,  suit: '♦' }),
+                    new Card({ value: 5,  suit: '♦' }),
+                    new Card({ value: 6,  suit: '♠' })
+                ];
                 expect(poker._isOnePair()).toEqual(false);
             });
         });
+        describe('without repeating Cards', function () {
+            it('should returns false', function () {
+                poker.board = [
+                    new Card({ value: 10, suit: '♥' }),
+                    new Card({ value: 3,  suit: '♥' }),
+                    new Card({ value: 2,  suit: '♦' }),
+                    new Card({ value: 0 }),
+                    new Card({ value: 6,  suit: '♠' })
+                ];
+                expect(poker._isOnePair()).toEqual(true);
+            });
+        });
         describe('with pair of Cards', function () {
-            poker.hands = [
-                new Card({ value: 10, suit: '♥' }),
-                new Card({ value: 3,  suit: '♥' }),
-                new Card({ value: 10, suit: '♦' }),
-                new Card({ value: 5,  suit: '♦' }),
-                new Card({ value: 6,  suit: '♠' })
-            ]
             it('should returns true', function () {
+                poker.board = [
+                    new Card({ value: 10, suit: '♥' }),
+                    new Card({ value: 3,  suit: '♥' }),
+                    new Card({ value: 10, suit: '♦' }),
+                    new Card({ value: 5,  suit: '♦' }),
+                    new Card({ value: 6,  suit: '♠' })
+                ];
                 expect(poker._isOnePair()).toEqual(true);
             });
         });
@@ -34,26 +46,26 @@ define([
 
     describe('Checking Hands with method _isTwoPair()', function () {
         describe('with one pair of Cards', function () {
-            poker.hands = [
-                new Card({ value: 10, suit: '♥' }),
-                new Card({ value: 3,  suit: '♥' }),
-                new Card({ value: 2,  suit: '♦' }),
-                new Card({ value: 5,  suit: '♦' }),
-                new Card({ value: 5,  suit: '♠' })
-            ]
             it('should returns false', function () {
+                poker.board = [
+                    new Card({ value: 10, suit: '♥' }),
+                    new Card({ value: 3,  suit: '♥' }),
+                    new Card({ value: 2,  suit: '♦' }),
+                    new Card({ value: 5,  suit: '♦' }),
+                    new Card({ value: 5,  suit: '♠' })
+                ];
                 expect(poker._isTwoPair()).toEqual(false);
             });
         });
         describe('with two pairs of Cards', function () {
-            poker.hands = [
-                new Card({ value: 10, suit: '♥' }),
-                new Card({ value: 3,  suit: '♥' }),
-                new Card({ value: 10, suit: '♦' }),
-                new Card({ value: 5,  suit: '♦' }),
-                new Card({ value: 3,  suit: '♠' })
-            ]
             it('should returns true', function () {
+                poker.board = [
+                    new Card({ value: 10, suit: '♥' }),
+                    new Card({ value: 3,  suit: '♥' }),
+                    new Card({ value: 10, suit: '♦' }),
+                    new Card({ value: 5,  suit: '♦' }),
+                    new Card({ value: 3,  suit: '♠' })
+                ];
                 expect(poker._isTwoPair()).toEqual(true);
             });
         });
