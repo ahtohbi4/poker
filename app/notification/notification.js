f9baf034e84347979a885a227ea8f9fe54f85d73
Alexander Antonov <alexandr-post@yandex.ru>
2016-02-28 21:22:48 +0300

    Notification was updated

--- app/notification/notification.js
+++ app/notification/notification.js
@@ -4,6 +4,9 @@ define([
     'use strict';
 
     function Notification() {
+        this._LIFETIME = 5000;
+        this._FADING_OUT_TIME = 1000;
+
         return this;
     }
 
@@ -20,11 +23,40 @@ define([
 
     /**
      * @method
+     * @returns {jQuery}
+     * @private
+     */
+    Notification.prototype._addMessage = function (text) {
+        var message = $('<div class="notification__message">').text(text);
+
+        var removeMessage = this._removeMessage(message);
+        setTimeout(removeMessage, this._LIFETIME);
+
+        return message;
+    };
+
+    /**
+     * @method
+     * @returns {function}
+     * @private
+     */
+    Notification.prototype._removeMessage = function (message) {
+        var _this = this;
+
+        return function () {
+            message.fadeOut(_this._FADING_OUT_TIME, function () {
+                message.remove();
+            });
+        };
+    };
+
+    /**
+     * @method
      * @returns {Notification}
      * @public
      */
-    Notification.prototype.send = function(message) {
-        this._getElem('container').html(message || '');
+    Notification.prototype.send = function (text) {
+        this._getElem('container').prepend(this._addMessage(text || ''));
 
         return this;
     };
