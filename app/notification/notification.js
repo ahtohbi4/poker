9f5d344dea1c7bbaa24d7c65b55cfe0487c2189a
Alexander Antonov <alexandr-post@yandex.ru>
2016-03-20 03:50:05 +0300

    Notification was updated

--- app/notification/notification.js
+++ app/notification/notification.js
@@ -3,9 +3,16 @@ define([
 ], function ($) {
     'use strict';
 
+    /**
+     * @class
+     * @property {number} _OPACITY_FOR_DISABLED
+     * @property {number} _LIFETIME_OF_DISABLED
+     * @property {number} _FADING_OUT_SPEED
+     */
     function Notification() {
-        this._LIFETIME = 5000;
-        this._FADING_OUT_TIME = 1000;
+        this._OPACITY_FOR_DISABLED = 0.3,
+        this._LIFETIME_OF_DISABLED = 5000;
+        this._FADING_OUT_SPEED = 1000;
 
         this.messages = [];
 
@@ -14,6 +21,7 @@ define([
 
     /**
      * @method
+     * @param {string} elem
      * @returns {jQuery}
      * @private
      */
@@ -25,50 +33,71 @@ define([
 
     /**
      * @method
+     * @param {string} text
      * @returns {jQuery}
      * @private
      */
     Notification.prototype._createMessage = function (text) {
-        var message = $('<div class="notification__message">').text(text);
+        var $message = $('<div class="notification__message">').text(text);
 
-        var removeMessage = this._removeMessage(message);
-        setTimeout(removeMessage, this._LIFETIME);
+        /* Initialization of fading out for last message */
+        this._disableMessage(this.messages[this.messages.length - 1]);
 
-        var lastMessage = this.messages[this.messages.length - 1];
-        if (typeof lastMessage !== 'undefined') {
-            lastMessage.animate({
-                opacity: 0.3
-            }, 1000);
-        }
-        this.messages.push(message);
+        this.messages.push($message);
 
-        return message;
+        return $message;
     };
 
     /**
      * @method
+     * @param {jQuery} $message
+     * @returns {boolean}
+     * @private
+     */
+    Notification.prototype._disableMessage = function ($message) {
+        var result = false;
+
+        if (typeof $message !== 'undefined') {
+            $message.animate({
+                opacity: this._OPACITY_FOR_DISABLED
+            }, this._FADING_OUT_SPEED);
+
+            var removingMessageInit = this._removeMessage($message);
+            setTimeout(removingMessageInit, this._LIFETIME_OF_DISABLED);
+
+            result = true;
+        }
+
+        return result;
+    };
+
+    /**
+     * @method
+     * @param {jQuery} $message
      * @returns {function}
      * @private
      */
-    Notification.prototype._removeMessage = function (message) {
+    Notification.prototype._removeMessage = function ($message) {
         var _this = this;
 
         return function () {
-            message.fadeOut(_this._FADING_OUT_TIME, function () {
-                message.remove();
+            $message.fadeOut(_this._FADING_OUT_SPEED, function () {
+                $message.remove();
+                _this.messages.shift();
             });
         };
     };
 
     /**
      * @method
+     * @param {string} text
      * @returns {Notification}
      * @public
      */
     Notification.prototype.send = function (text) {
-        var message = this._createMessage(text || '');
+        var $message = this._createMessage(text || '');
 
-        this._getElem('container').append(message);
+        this._getElem('container').append($message);
 
         return this;
     };
